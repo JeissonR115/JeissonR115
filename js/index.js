2916f8ce2e34d308f109668a2b16c8bbfd5797be
jeissonR115 <jeissonfeliper@gmail.com>
2023-05-25 18:24:20 -0500

    simplifico

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
-function cambiaTema(ev) {
-    ev.target.checked ? theme = 'light' : theme = 'dark';
-    document.documentElement.setAttribute('theme', theme);
-}
-colorSwitch.addEventListener('change', cambiaTema);
